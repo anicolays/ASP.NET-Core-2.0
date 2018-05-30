acaca2a14db11d9cf173ad2a57442a543348a07b
Mykola <antomykl@gmail.com>
2018-05-30 17:01:38 +0300

    Paging  Student on Index

--- Razor_Pages-EF_Core/Pages/Students/Index.cshtml.cs
+++ Razor_Pages-EF_Core/Pages/Students/Index.cshtml.cs
@@ -24,12 +24,24 @@ namespace Razor_Pages_EF_Core.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Student { get; set; }
+        public PaginatedList<Student> Student { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string searchString,
+         string currentFilter, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
             CurrentFilter = searchString;
 
             IQueryable<Student> studentIQ = from s in _context.Students select s;
@@ -56,9 +68,9 @@ namespace Razor_Pages_EF_Core.Pages.Students
                     break;
             }
 
-
-            Student = await studentIQ
-            .AsNoTracking().ToListAsync();
+            int pageSize = 3;
+            Student = await PaginatedList<Student>.CreateAsynq(
+            studentIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
