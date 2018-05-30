e4276dd3d03198bdc21f7ed0227cb62ab920b654
Mykola <antomykl@gmail.com>
2018-05-30 17:32:22 +0300

    About Page EF Statistics

---
+++ Razor_Pages-EF_Core/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Razor_Pages_EF_Core.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
--- Razor_Pages-EF_Core/Pages/About.cshtml.cs
+++ Razor_Pages-EF_Core/Pages/About.cshtml.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Razor_Pages_EF_Core.Data;
+using Razor_Pages_EF_Core.Models.SchoolViewModels;
 
 namespace Razor_Pages_EF_Core.Pages
 {
     public class AboutModel : PageModel
     {
-        public string Message { get; set; }
 
-        public void OnGet()
+        private readonly SchoolContext _context;
+
+        public AboutModel(SchoolContext context)
         {
-            Message = "Your application description page.";
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Student { get; set; }
+
+
+
+
+        public async Task OnGet()
+        {
+            IQueryable<EnrollmentDateGroup> data = 
+            from student in _context.Students
+            group student by student.EnrollmentDate into dateGroup 
+            select new EnrollmentDateGroup()
+            {
+                EnrollmentDate = dateGroup.Key,
+                StudentCount = dateGroup.Count()
+            };
+
+            Student = await data.AsNoTracking().ToListAsync();
         }
     }
 }
